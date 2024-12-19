29ce45d984b5b4de4059600b32f4429dfbc90426
PhongPhamj <phongvupham1110@gmail.com>
2024-12-19 10:06:42 +0700

    Update auto.tf

--- base/auto.tf
+++ base/auto.tf
@@ -1,17 +1,18 @@
 locals {
   yaml_files = fileset("./resources", "*.yaml")
-
-  config = merge([
-    for file_path in local.yaml_files : yamldecode(file((join("", ["./resources/", file_path]))))
-  ]...)
+  
+  configs = {
+    for file_path in local.yaml_files :
+    file_path => yamldecode(file(join("", ["./resources/", file_path])))
+  }
 }
 
 resource "aws_s3_bucket" "this" {
-  count               = length(local.config)
-  bucket              = local.config[count.index].bucket
-  force_destroy       = local.config[count.index].force_destroy
-  object_lock_enabled = local.config[count.index].object_lock_enabled
-  tags                = merge(local.config[count.index].tags, {})
+  for_each            = local.configs
+  bucket              = each.value.bucket
+  force_destroy       = each.value.force_destroy
+  object_lock_enabled = each.value.object_lock_enabled
+  tags                = merge(each.value.tags, {})
 
   lifecycle {
     ignore_changes = [
@@ -21,31 +22,39 @@ resource "aws_s3_bucket" "this" {
 }
 
 resource "aws_s3_bucket_ownership_controls" "this" {
-  count  = length(local.config)
-  bucket = aws_s3_bucket.this[count.index].id
+  for_each = local.configs
+  bucket   = aws_s3_bucket.this[each.key].id
+
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
+
 resource "aws_s3_bucket_acl" "this" {
-  count      = length(local.config)
-  bucket     = aws_s3_bucket.this[count.index].id
-  acl        = local.config[count.index].acl
-  depends_on = [aws_s3_bucket_public_access_block.this, aws_s3_bucket_ownership_controls.this]
+  for_each   = local.configs
+  bucket     = aws_s3_bucket.this[each.key].id
+  acl        = each.value.acl
+  depends_on = [
+    aws_s3_bucket_public_access_block.this,
+    aws_s3_bucket_ownership_controls.this,
+  ]
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
-  count                   = length(local.config)
-  bucket                  = aws_s3_bucket.this[count.index].id
-  block_public_acls       = try(lower(local.config[count.index].acl), null) == "private" ? true : false
-  block_public_policy     = try(lower(local.config[count.index].acl), null) == "private" ? true : false
-  ignore_public_acls      = try(lower(local.config[count.index].acl), null) == "private" ? true : false
-  restrict_public_buckets = try(lower(local.config[count.index].acl), null) == "private" ? true : false
+  for_each = local.configs
+  bucket   = aws_s3_bucket.this[each.key].id
+
+  block_public_acls      = try(lower(each.value.acl), null) == "private" ? true : false
+  block_public_policy    = try(lower(each.value.acl), null) == "private" ? true : false
+  ignore_public_acls     = try(lower(each.value.acl), null) == "private" ? true : false
+  restrict_public_buckets = try(lower(each.value.acl), null) == "private" ? true : false
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.this[count.index].id
+  for_each = local.configs
+  bucket   = aws_s3_bucket.this[each.key].id
+
   versioning_configuration {
-    status = local.config[count.index].enable_versioning ? "Enabled" : "Suspended"
+    status = each.value.enable_versioning ? "Enabled" : "Suspended"
   }
 }
